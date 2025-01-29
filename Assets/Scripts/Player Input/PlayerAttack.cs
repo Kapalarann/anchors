d44a1f203e05e436a58e90b7229802d8d657f928
John Mervic Bautista <167274375+gradiel9901@users.noreply.github.com>
2025-01-29 23:50:35 +0800

    Bautista_Melee
    1. Movement Animation
    2. Camera 3rd Person View
    3. Idle Animation
    4. Attack Animation

---
+++ Assets/Scripts/Player Input/PlayerAttack.cs
@@ -0,0 +1,43 @@
+﻿using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator _animator;
+    //private bool _isAttacking = false;
+   [SerializeField] AnimationReceiver _receiver;
+    MeleeMovement _meleeMovement;
+
+    private static readonly int OnAttackHash = Animator.StringToHash("OnAttack");
+
+    private void Awake()
+    {
+        _meleeMovement = GetComponent<MeleeMovement>();
+        _animator = GetComponent<Animator>();
+        _receiver.AttackEnd += EndAttack;
+    }
+
+    public void Attack_Event(InputAction.CallbackContext context)
+    {
+        if (context.performed && !_meleeMovement._isAttacking)
+        {
+            Debug.Log("Attack button pressed!");
+            _animator.SetTrigger(OnAttackHash); // ✅ Triggers "OnAttack"
+            _meleeMovement._isAttacking = true;
+        }
+    }
+
+        
+    private void OnDestroy()
+    {
+        _receiver.AttackEnd -= EndAttack;
+    }
+
+    // This function is called from AnimationReceiver when the attack animation ends
+    public void EndAttack(AnimationEvent animationEvent)
+    {
+        Debug.Log("Resetting attack state.");
+        _meleeMovement._isAttacking = false;
+    }
+}
